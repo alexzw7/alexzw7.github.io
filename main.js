b7f1ae7fb13769a11cee5c7e0ce9090272933c41
alexzw <alexzw@umich.edu>
2019-11-22 05:00:30 -0500

    added first version of blur

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ function backImage() {
     var subHeight = mainNav.offsetHeight;
     jumbo.style.height = "calc(100vh - " + subHeight + "px)";
 }
+
+setTimeout(function(){document.getElementById("jumbo").style.backgroundImage = "url(blurred1.jpg)";},900);
+setTimeout(function(){document.getElementById("delayed").style.visibility = "visible";},1500);
