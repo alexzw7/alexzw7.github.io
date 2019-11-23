b94ee75ba84a42313de5a06d352abdb89ec92c91
alexzw <alexzw@umich.edu>
2019-11-22 20:43:48 -0500

    another one

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function backImage() {
+/*function backImage() {
     var jumbo = document.getElementById("jumbo");
     var mainNav = document.getElementById("mainNav");
     var subHeight = mainNav.offsetHeight;
     jumbo.style.height = "calc(100vh - " + subHeight + "px)";
-}
+}*/
 
 setTimeout(function(){document.getElementById("jumbo").style.backgroundImage = "url(blurred1.jpg)";},900);
 setTimeout(function(){document.getElementById("delayed").style.visibility = "visible";},1500);
