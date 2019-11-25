32cbc3aa88bbb526a9504ea011d82320fb9ad5ee
alexzw <alexzw@umich.edu>
2019-11-25 02:14:34 -0500

    big changes

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-/*function backImage() {
-    var jumbo = document.getElementById("jumbo");
-    var mainNav = document.getElementById("mainNav");
-    var subHeight = mainNav.offsetHeight;
-    jumbo.style.height = "calc(100vh - " + subHeight + "px)";
-}*/
-
 setTimeout(function(){document.getElementById("jumbo").style.backgroundImage = "url(blurred1.jpg)";},900);
 setTimeout(function(){document.getElementById("delayed").style.visibility = "visible";},1500);
 
@@ -12,6 +5,7 @@ function changeMode() {
     var main = document.getElementById("MAINCONTAINER");
     var sub = document.getElementById("SUBCONTAINER");
     var mode = document.getElementById("MODE");
+    var navColor = document.getElementById("mainNav");
     var sep1 = document.getElementById("SEP1");
     var sep2 = document.getElementById("SEP2");
     var sep3 = document.getElementById("SEP3");
@@ -21,6 +15,7 @@ function changeMode() {
         main.className = "container-fluid bg-dark";
         sub.className = "col-lg-6 col-md-6 col-sm-12 col-xs-12 my-0 bg-dark text-white";
         mode.style.color = "white";
+        navColor.className = "navbar navbar-expand-md navbar-dark bg-dark sticky-top";
         sep1.className = "bg-white";
         sep2.className = "bg-white";
         sep3.className = "bg-white";
@@ -30,6 +25,7 @@ function changeMode() {
         main.className = "container-fluid bg-white";
         sub.className = "col-lg-6 col-md-6 col-sm-12 col-xs-12my-0 bg-white text-black";
         mode.style.color = "black";
+        navColor.className = "navbar navbar-expand-md navbar-light bg-white sticky-top";
         sep1.className = "bg-dark";
         sep2.className = "bg-dark";
         sep3.className = "bg-dark";
